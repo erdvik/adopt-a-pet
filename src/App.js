768d47c0acc24865d51603e69dc10cf13a6d5f41
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-01-11 19:30:19 +0100

    Import react router and wrap components in app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <HomePage />
+      <Router>
+        <Navigation />
+        <HomePage />
+      </Router>
     </div>
   );
 }
